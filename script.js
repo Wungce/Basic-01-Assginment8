3b5c9376e0c26ce8c1eac767d192450ed7ca9f5f
wangsh <wugcelama19@gmail.com>
2021-06-11 13:37:50 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const number = 6 + 6;
+alert(number) ;
+
+const myStringNumber = "I have two number :" + " 2";
+alert(myStringNumber) ;
+
+const myString = "I am : " + "Hello, " + "Hi.";
+alert(myString) ;
